52886260765f432937d2dc61145cf8dd264c4b95
Sanjay-RD <rishidevsanjay9@gmail.com>
2021-05-08 10:37:22 +0545

    Pages and Routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About page</h1>
+      <p>This is an app to find latest DJ and other musical events</p>
+      <p>Version 1.0.0</p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+const EventPage = () => {
+  return (
+    <div>
+      <h1>Event page</h1>
+    </div>
+  );
+};
+
+export default EventPage;
