4179ac249856f9fc65a84e8625d7be03aa47b13f
Sanjay-RD <rishidevsanjay9@gmail.com>
2021-05-09 08:45:52 +0545

    Nextlink and Router

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
 const AboutPage = () => {
   return (
     <div>
       <h1>About page</h1>
       <p>This is an app to find latest DJ and other musical events</p>
       <p>Version 1.0.0</p>
+      <Link href="/">Back</Link>
     </div>
   );
 };
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,12 @@
+import { useRouter } from "next/router";
 const EventPage = () => {
+  const router = useRouter();
+  console.log(router);
   return (
     <div>
-      <h1>Event page</h1>
+      <h1>My Event page</h1>
+      <h2>my events params : {router.query.slug}</h2>
+      <button onClick={() => router.push("/")}>Home Page </button>
     </div>
   );
 };
